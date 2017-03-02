e01c94c20e7231e2cba3d67184f2716cc1c03f08
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-02 13:37:23 -0500

    Make changes to search hook:
    - Community is using queryable, hence the change in variable name from
    query to queryable
    - Add case_insensitive macro to default search hook for future use

--- lib/rummage_ecto/hooks/search.ex
+++ lib/rummage_ecto/hooks/search.ex
@@ -12,63 +12,69 @@ defmodule Rummage.Ecto.Hooks.Search do
   @behaviour Rummage.Ecto.Hook
 
   @doc """
-  Builds a search query on top of the given `query` from the rummage parameters
+  Builds a search queryable on top of the given `queryable` from the rummage parameters
   from the given `rummage` struct.
 
   ## Examples
   When rummage struct passed doesn't have the key "search", it simply returns the
-  query itself:
+  queryable itself:
 
       iex> alias Rummage.Ecto.Hooks.Search
       iex> import Ecto.Query
       iex> Search.run(Parent, %{})
       Parent
 
-  When the query passed is not just a struct:
+  When the queryable passed is not just a struct:
 
       iex> alias Rummage.Ecto.Hooks.Search
       iex> import Ecto.Query
-      iex> query = from u in "parents"
+      iex> queryable = from u in "parents"
       #Ecto.Query<from p in "parents">
-      iex>  Search.run(query, %{})
+      iex>  Search.run(queryable, %{})
       #Ecto.Query<from p in "parents">
 
   When rummage struct passed has the key "search", with "field" and "term"
-  it returns a searched version of the query passed in as the argument:
+  it returns a searched version of the queryable passed in as the argument:
 
       iex> alias Rummage.Ecto.Hooks.Search
       iex> import Ecto.Query
       iex> rummage = %{"search" => %{"field_1" => "field_!"}}
       %{"search" => %{"field_1" => "field_!"}}
-      iex> query = from u in "parents"
+      iex> queryable = from u in "parents"
       #Ecto.Query<from p in "parents">
-      iex> Search.run(query, rummage)
+      iex> Search.run(queryable, rummage)
       #Ecto.Query<from p in "parents", where: like(p.field_1, ^"%field_!%")>
   """
   @spec run(Ecto.Query.t, map) :: {Ecto.Query.t, map}
-  def run(query, rummage) do
+  def run(queryable, rummage) do
     search_params = Map.get(rummage, "search")
 
     case search_params do
-      a when a in [nil, [], ""] -> query
-      _ -> handle_search(query, search_params)
+      a when a in [nil, [], ""] -> queryable
+      _ -> handle_search(queryable, search_params)
     end
   end
 
-  defp handle_search(query, search_params) do
-    search_params
-    |> Map.to_list
-    |> Enum.reduce(query, &make_search_query(&1, &2))
+  defmacrop case_insensitive(field) do
+    quote do
+      fragment("lower(?)", unquote(field))
+    end
   end
 
-  defp make_search_query(param, query) do
+  defp handle_search(queryable, search_params) do
+    search_params
+    |> Map.to_list
+    |> Enum.reduce(queryable, &make_search_queryable(&1, &2))
+  end
+
+  defp make_search_queryable(param, queryable) do
     field = param
       |> elem(0)
       |> String.to_atom
 
     term = elem(param, 1)
 
-    query
+    queryable
     |> where([b],
       like(field(b, ^field), ^"%#{String.replace(term, "%", "\\%")}%"))
   end
