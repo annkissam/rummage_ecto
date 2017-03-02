98315f1d48917c7f612403157152043de40a15c8
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-02 13:24:44 -0500

    Add documentation to runtime config getters

--- lib/rummage_ecto/config.ex
+++ lib/rummage_ecto/config.ex
@@ -1,43 +1,54 @@
 defmodule Rummage.Ecto.Config do
-  @moduledoc false
+  @moduledoc """
+  This module encapsulates all the Rummage's runtime configurations
+  that can be set in the config.exs file.
+  """
 
-  @doc false
+  @doc """
+  `:default_search` hook can also be set at run time
+  in the config.exs file
+  """
   def default_search do
     config(:default_search, Rummage.Ecto.Hooks.Search)
   end
 
-  @doc false
+  @doc """
+  `:default_sort` hook can also be set at run time
+  in the config.exs file
+  """
   def default_sort do
     config(:default_sort, Rummage.Ecto.Hooks.Sort)
   end
 
-  @doc false
+  @doc """
+  `:default_paginate` hook can also be set at run time
+  in the config.exs file
+  """
   def default_paginate do
     config(:default_paginate, Rummage.Ecto.Hooks.Paginate)
   end
 
-  @doc false
+  @doc """
+  `:default_per_page` hook can also be set at run time
+  in the config.exs file
+  """
   def default_per_page do
     config(:default_per_page, "10")
   end
 
-  @doc false
   defp config do
     Application.get_env(:rummage_ecto, Rummage.Ecto, [])
   end
 
-  @doc false
   defp config(key, default) do
     config()
     |> Keyword.get(key, default)
     |> resolve_config(default)
   end
 
-  @doc false
   defp resolve_config({:system, var_name}, default) do
     System.get_env(var_name) || default
   end
 
-  @doc false
   defp resolve_config(value, _default), do: value
 end
