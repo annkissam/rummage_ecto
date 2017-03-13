59f204816ee53a337e2848654bd7db7ace541684
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-13 17:11:57 -0400

    wip

--- lib/rummage_ecto.ex
+++ lib/rummage_ecto.ex
@@ -33,6 +33,104 @@ defmodule Rummage.Ecto do
   """
 
   alias Rummage.Ecto.Config
+  import Ecto.Query
+
+  @spec rummage(Ecto.Query.t, map, map) :: {Ecto.Query.t, map}
+  def rummage(queryable, rummage, opts \\ %{}) when is_nil(rummage) or rummage == %{} do
+    params = %{"search" => %{},
+      "sort"=> [],
+      "paginate" => %{"per_page" => default_per_page(), "page" => "1"},
+    }
+
+    rummage = before_paginate(queryable, params)
+
+    queryable = queryable
+      |> paginate_hook_call(rummage)
+
+    {queryable, rummage}
+  end
+
+  def rummage(queryable, rummage) do
+    searched_queryable = queryable
+      |> search_hook_call(rummage)
+
+    rummage = before_paginate(searched_queryable, rummage)
+
+    rummaged_queryable = searched_queryable
+      |> sort_hook_call(rummage)
+      |> paginate_hook_call(rummage)
+
+    {rummaged_queryable, rummage}
+  end
+
+  defp before_paginate(queryable, rummage) do
+    paginate_params = Map.get(rummage, "paginate")
+
+    case paginate_params do
+      nil -> rummage
+      _ ->
+        total_count = get_total_count(queryable)
+
+        {page, per_page} = parse_page_and_per_page(paginate_params)
+
+        per_page = if per_page < 1, do: 1, else: per_page
+
+        max_page_fl = total_count / per_page
+        max_page = max_page_fl
+          |> Float.ceil
+          |> round
+
+        page = cond do
+          page < 1 ->  1
+          max_page > 0 && page > max_page -> max_page
+          true -> page
+        end
+
+        paginate_params = paginate_params
+          |> Map.put("page", Integer.to_string(page))
+          |> Map.put("per_page", Integer.to_string(per_page))
+          |> Map.put("total_count", Integer.to_string(total_count))
+          |> Map.put("max_page", Integer.to_string(max_page))
+
+        Map.put(rummage, "paginate", paginate_params)
+    end
+  end
+
+  defp get_total_count(queryable) do
+    repo = get_repo()
+
+    queryable = queryable
+      |> select([b], count(b.id))
+
+    apply(repo, :one, [queryable])
+  end
+
+  defp get_repo do
+    unquote(opts[:repo]) ||
+      Config.default_repo ||
+      make_repo_name_from_topmost_namespace
+  end
+
+  defp make_repo_name_from_topmost_namespace do
+    "#{__MODULE__}"
+    |> String.split(".")
+    |> Enum.at(1)
+    |> (& "Elixir." <> &1 <> ".Repo").()
+    |> String.to_atom
+  end
+
+  defp parse_page_and_per_page(paginate_params) do
+    per_page = paginate_params
+      |> Map.get("per_page", default_per_page())
+      |> String.to_integer
+
+    page = paginate_params
+      |> Map.get("page", "1")
+      |> String.to_integer
+
+    {page, per_page}
+  end
+
 
   defmacro __using__(opts) do
     quote do
