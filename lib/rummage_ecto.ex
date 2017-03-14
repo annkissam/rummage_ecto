b1503cd96a88772f1e274b4d1468c2ef99c801d0
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-13 21:32:46 -0400

    Remove __using__ macro:
    - Going functional route based on suggestions I got from the Elixir Community
    - Putting a lot code in __using__ is not considered a good practice: https://hexdocs.pm/elixir/Kernel.html#use/2-best-practices

--- lib/rummage_ecto.ex
+++ lib/rummage_ecto.ex
@@ -17,14 +17,14 @@ defmodule Rummage.Ecto do
   ```elixir
   defmodule Rummage.Ecto.Product do
     use Ecto.Schema
-    use Rummage.Ecto # Can pass options if need be
+
   end
   ```
 
   This allows you to do:
 
       iex> rummage = %{"search" => %{"name" => %{"assoc" => [], "search_type" => "ilike", "search_term" => "field_!"}}}
-      iex> {queryable, rummage} = Rummage.Ecto.Product.rummage(Rummage.Ecto.Product, rummage)
+      iex> {queryable, rummage} = Rummage.Ecto.rummage(Rummage.Ecto.Product, rummage)
       iex> queryable
       #Ecto.Query<from p in Rummage.Ecto.Product, where: ilike(p.name, ^"%field_!%")>
       iex> rummage
@@ -35,218 +35,33 @@ defmodule Rummage.Ecto do
   alias Rummage.Ecto.Config
   import Ecto.Query
 
+  # @spec rummage(Ecto.Query.t, map, map) :: {Ecto.Query.t, map}
+  # def rummage(queryable, rummage, opts \\ %{}) when is_nil(rummage) or rummage == %{} do
+  #   params = %{"search" => %{},
+  #     "sort"=> [],
+  #     "paginate" => %{"per_page" => default_per_page(), "page" => "1"},
+  #   }
+
+  #   hooks = opts[:hooks] || [:search, :sort, :paginate]
+
+  #   rummage = before_paginate(queryable, params)
+
+  #   queryable = queryable
+  #     |> paginate_hook_call(rummage)
+
+  #   {queryable, rummage}
+  # end
+
   @spec rummage(Ecto.Query.t, map, map) :: {Ecto.Query.t, map}
-  def rummage(queryable, rummage, opts \\ %{}) when is_nil(rummage) or rummage == %{} do
-    params = %{"search" => %{},
-      "sort"=> [],
-      "paginate" => %{"per_page" => default_per_page(), "page" => "1"},
-    }
+  def rummage(queryable, rummage, opts \\ %{}) do
+    hooks = opts[:hooks] || [:search, :sort, :paginate]
 
-    rummage = before_paginate(queryable, params)
+    Enum.reduce(hooks, {queryable, rummage}, fn(hook, {q, r}) ->
+      hook_module = opts[hook] || apply(Config, String.to_atom("default_#{hook}"), [])
 
-    queryable = queryable
-      |> paginate_hook_call(rummage)
+      rummage = hook_module.before_hook(q, r, opts)
 
-    {queryable, rummage}
-  end
-
-  def rummage(queryable, rummage) do
-    searched_queryable = queryable
-      |> search_hook_call(rummage)
-
-    rummage = before_paginate(searched_queryable, rummage)
-
-    rummaged_queryable = searched_queryable
-      |> sort_hook_call(rummage)
-      |> paginate_hook_call(rummage)
-
-    {rummaged_queryable, rummage}
-  end
-
-  defp before_paginate(queryable, rummage) do
-    paginate_params = Map.get(rummage, "paginate")
-
-    case paginate_params do
-      nil -> rummage
-      _ ->
-        total_count = get_total_count(queryable)
-
-        {page, per_page} = parse_page_and_per_page(paginate_params)
-
-        per_page = if per_page < 1, do: 1, else: per_page
-
-        max_page_fl = total_count / per_page
-        max_page = max_page_fl
-          |> Float.ceil
-          |> round
-
-        page = cond do
-          page < 1 ->  1
-          max_page > 0 && page > max_page -> max_page
-          true -> page
-        end
-
-        paginate_params = paginate_params
-          |> Map.put("page", Integer.to_string(page))
-          |> Map.put("per_page", Integer.to_string(per_page))
-          |> Map.put("total_count", Integer.to_string(total_count))
-          |> Map.put("max_page", Integer.to_string(max_page))
-
-        Map.put(rummage, "paginate", paginate_params)
-    end
-  end
-
-  defp get_total_count(queryable) do
-    repo = get_repo()
-
-    queryable = queryable
-      |> select([b], count(b.id))
-
-    apply(repo, :one, [queryable])
-  end
-
-  defp get_repo do
-    unquote(opts[:repo]) ||
-      Config.default_repo ||
-      make_repo_name_from_topmost_namespace
-  end
-
-  defp make_repo_name_from_topmost_namespace do
-    "#{__MODULE__}"
-    |> String.split(".")
-    |> Enum.at(1)
-    |> (& "Elixir." <> &1 <> ".Repo").()
-    |> String.to_atom
-  end
-
-  defp parse_page_and_per_page(paginate_params) do
-    per_page = paginate_params
-      |> Map.get("per_page", default_per_page())
-      |> String.to_integer
-
-    page = paginate_params
-      |> Map.get("page", "1")
-      |> String.to_integer
-
-    {page, per_page}
-  end
-
-
-  defmacro __using__(opts) do
-    quote do
-      import Ecto.Query
-
-      @spec rummage(Ecto.Query.t, map) :: {Ecto.Query.t, map}
-      def rummage(queryable, rummage) when is_nil(rummage) or rummage == %{} do
-        params = %{"search" => %{},
-          "sort"=> [],
-          "paginate" => %{"per_page" => default_per_page(), "page" => "1"},
-        }
-
-        rummage = before_paginate(queryable, params)
-
-        queryable = queryable
-          |> paginate_hook_call(rummage)
-
-        {queryable, rummage}
-      end
-
-      def rummage(queryable, rummage) do
-        searched_queryable = queryable
-          |> search_hook_call(rummage)
-
-        rummage = before_paginate(searched_queryable, rummage)
-
-        rummaged_queryable = searched_queryable
-          |> sort_hook_call(rummage)
-          |> paginate_hook_call(rummage)
-
-        {rummaged_queryable, rummage}
-      end
-
-      def default_per_page do
-        unquote(Integer.to_string(opts[:per_page] || Config.default_per_page))
-      end
-
-      defp search_hook_call(queryable, rummage) do
-        unquote(opts[:search_hook] || Config.default_search).run(queryable, rummage)
-      end
-
-      defp sort_hook_call(queryable, rummage) do
-        unquote(opts[:sort_hook] || Config.default_sort).run(queryable, rummage)
-      end
-
-      defp paginate_hook_call(queryable, rummage) do
-        unquote(opts[:paginate_hook] || Config.default_paginate).run(queryable, rummage)
-      end
-
-      defp before_paginate(queryable, rummage) do
-        paginate_params = Map.get(rummage, "paginate")
-
-        case paginate_params do
-          nil -> rummage
-          _ ->
-            total_count = get_total_count(queryable)
-
-            {page, per_page} = parse_page_and_per_page(paginate_params)
-
-            per_page = if per_page < 1, do: 1, else: per_page
-
-            max_page_fl = total_count / per_page
-            max_page = max_page_fl
-              |> Float.ceil
-              |> round
-
-            page = cond do
-              page < 1 ->  1
-              max_page > 0 && page > max_page -> max_page
-              true -> page
-            end
-
-            paginate_params = paginate_params
-              |> Map.put("page", Integer.to_string(page))
-              |> Map.put("per_page", Integer.to_string(per_page))
-              |> Map.put("total_count", Integer.to_string(total_count))
-              |> Map.put("max_page", Integer.to_string(max_page))
-
-            Map.put(rummage, "paginate", paginate_params)
-        end
-      end
-
-      defp get_total_count(queryable) do
-        repo = get_repo()
-
-        queryable = queryable
-          |> select([b], count(b.id))
-
-        apply(repo, :one, [queryable])
-      end
-
-      defp get_repo do
-        unquote(opts[:repo]) ||
-          Config.default_repo ||
-          make_repo_name_from_topmost_namespace
-      end
-
-      defp make_repo_name_from_topmost_namespace do
-        "#{__MODULE__}"
-        |> String.split(".")
-        |> Enum.at(1)
-        |> (& "Elixir." <> &1 <> ".Repo").()
-        |> String.to_atom
-      end
-
-      defp parse_page_and_per_page(paginate_params) do
-        per_page = paginate_params
-          |> Map.get("per_page", default_per_page())
-          |> String.to_integer
-
-        page = paginate_params
-          |> Map.get("page", "1")
-          |> String.to_integer
-
-        {page, per_page}
-      end
-    end
+      {q |> hook_module.run(rummage), rummage}
+    end)
   end
 end
