9ca7cec01aad3f9c6074d529d6deae7b560f3a8f
Adi Iyengar <aditya7iyengar@gmail.com>
2017-03-13 21:56:19 -0400

    Make this work with new Rummage.Ecto:
    - Move before_paginate from rummage_ecto to paginate's before_hook
    - Add doc tests to it
    - Change total_count to count the length of the enum instead of query's count

--- lib/rummage_ecto/hooks/paginate.ex
+++ lib/rummage_ecto/hooks/paginate.ex
@@ -113,6 +113,79 @@ defmodule Rummage.Ecto.Hooks.Paginate do
     end
   end
 
+  @doc """
+  Implementation of `before_hook` for `Rummage.Ecto.Hooks.Paginate`. This function
+  takes a `queryable`, `rummage` struct and an `opts` map. Using those it calculates
+  the `total_count` and `max_page` for the paginate hook.
+
+  ## Examples
+      iex> alias Rummage.Ecto.Hooks.Paginate
+      iex> alias Rummage.Ecto.Category
+      iex> Paginate.before_hook(Category, %{}, %{})
+      %{}
+
+      iex> alias Rummage.Ecto.Hooks.Paginate
+      iex> alias Rummage.Ecto.Category
+      iex> Ecto.Adapters.SQL.Sandbox.checkout(Rummage.Ecto.Repo)
+      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 1"})
+      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 2"})
+      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 3"})
+      iex> rummage = %{"paginate" => %{"per_page" => "1", "page" => "1"}}
+      iex> Paginate.before_hook(Category, rummage, %{})
+      %{"paginate" => %{"max_page" => "3", "page" => "1", "per_page" => "1", "total_count" => "3"}}
+  """
+  def before_hook(queryable, rummage, opts) do
+    paginate_params = Map.get(rummage, "paginate")
+
+    case paginate_params do
+      nil -> rummage
+      _ ->
+        total_count = get_total_count(queryable, opts)
+
+        {page, per_page} = parse_page_and_per_page(paginate_params, opts)
+
+        per_page = if per_page < 1, do: 1, else: per_page
+
+        max_page_fl = total_count / per_page
+        max_page = max_page_fl
+          |> Float.ceil
+          |> round
+
+        page = cond do
+          page < 1 ->  1
+          max_page > 0 && page > max_page -> max_page
+          true -> page
+        end
+
+        paginate_params = paginate_params
+          |> Map.put("page", Integer.to_string(page))
+          |> Map.put("per_page", Integer.to_string(per_page))
+          |> Map.put("total_count", Integer.to_string(total_count))
+          |> Map.put("max_page", Integer.to_string(max_page))
+
+        Map.put(rummage, "paginate", paginate_params)
+    end
+  end
+
+  defp get_total_count(queryable, opts), do: length(apply(get_repo(opts), :all, [queryable]))
+
+  defp get_repo(opts) do
+    opts[:repo] ||
+      Rummage.Ecto.Config.default_repo
+  end
+
+  defp parse_page_and_per_page(paginate_params, opts) do
+    per_page = paginate_params
+      |> Map.get("per_page", Integer.to_string(opts[:per_page] || Rummage.Ecto.Config.default_per_page))
+      |> String.to_integer
+
+    page = paginate_params
+      |> Map.get("page", "1")
+      |> String.to_integer
+
+    {page, per_page}
+  end
+
   defp handle_paginate(queryable, paginate_params) do
     per_page = paginate_params
       |> Map.get("per_page")
