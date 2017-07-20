677afa151fac11288378b0601fc2c931ae1ff17d
Adi Iyengar <aditya7iyengar@gmail.com>
2017-07-19 23:34:14 -0400

    More updates

--- lib/rummage_ecto.ex
+++ lib/rummage_ecto.ex
@@ -99,7 +99,7 @@ defmodule Rummage.Ecto do
   """
   @spec rummage(Ecto.Query.t, map, map) :: {Ecto.Query.t, map}
   def rummage(queryable, rummage, opts \\ %{})
-  def rummage(queryable, rummage, opts) when rummage == nil, do: {queryable, %{}}
+  def rummage(queryable, rummage, _opts) when rummage == nil, do: {queryable, %{}}
   def rummage(queryable, rummage, opts) do
     hooks = opts[:hooks] || [:search, :sort, :paginate]
 
