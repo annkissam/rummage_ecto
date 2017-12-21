83302fdf0da3ee9ffda1dcec48a65cd91987f993
Adi <aditya7iyengar@gmail.com>
2017-12-21 10:29:55 -0500

    Change default_#{key} to #{key}

--- lib/rummage_ecto/config.ex
+++ lib/rummage_ecto/config.ex
@@ -5,78 +5,78 @@ defmodule Rummage.Ecto.Config do
   """
 
   @doc """
-  `:default_search` hook can also be set at run time
+  `:search` hook can also be set at run time
   in the `config.exs` file
 
   ## Examples
   When no config is set, if returns the default hook
   (`Rummage.Ecto.Hooks.Search`):
       iex> alias Rummage.Ecto.Config
-      iex> Config.default_search
+      iex> Config.search
       Rummage.Ecto.Hooks.Search
   """
-  def default_search do
-    config(:default_search, Rummage.Ecto.Hooks.Search)
+  def search do
+    config(:search, Rummage.Ecto.Hooks.Search)
   end
 
   @doc """
-  `:default_sort` hook can also be set at run time
+  `:sort` hook can also be set at run time
   in the `config.exs` file
 
   ## Examples
   When no config is set, if returns the default hook
   (`Rummage.Ecto.Hooks.Sort`):
       iex> alias Rummage.Ecto.Config
-      iex> Config.default_sort
+      iex> Config.sort
       Rummage.Ecto.Hooks.Sort
   """
-  def default_sort do
-    config(:default_sort, Rummage.Ecto.Hooks.Sort)
+  def sort do
+    config(:sort, Rummage.Ecto.Hooks.Sort)
   end
 
   @doc """
-  `:default_paginate` hook can also be set at run time
+  `:paginate` hook can also be set at run time
   in the `config.exs` file
 
   ## Examples
   When no config is set, if returns the default hook
   (`Rummage.Ecto.Hooks.Paginate`):
       iex> alias Rummage.Ecto.Config
-      iex> Config.default_paginate
+      iex> Config.paginate
       Rummage.Ecto.Hooks.Paginate
   """
-  def default_paginate do
-    config(:default_paginate, Rummage.Ecto.Hooks.Paginate)
+  def paginate do
+    config(:paginate, Rummage.Ecto.Hooks.Paginate)
   end
 
   @doc """
-  `:default_per_page` can also be set at run time
+  `:per_page` can also be set at run time
   in the `config.exs` file
 
   ## Examples
   Returns default `Repo` set in the config
   (`2 in `rummage_ecto`'s test env):
       iex> alias Rummage.Ecto.Config
-      iex> Config.default_per_page
+      iex> Config.per_page
       2
   """
-  def default_per_page do
-    config(:default_per_page, 10)
+  def per_page do
+    config(:per_page, 10)
   end
 
   @doc """
-  `:default_repo` can also be set at run time
+  `:repo` can also be set at run time
   in the config.exs file
 
   ## Examples
   Returns default `Repo` set in the config
   (`Rummage.Ecto.Repo` in `rummage_ecto`'s test env):
       iex> alias Rummage.Ecto.Config
-      iex> Config.default_repo
+      iex> Config.repo
       Rummage.Ecto.Repo
   """
-  def default_repo do
-    config(:default_repo, nil)
+  def repo do
+    config(:repo, nil)
   end
 
   @doc """
