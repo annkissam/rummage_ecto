ee11491066b7f3d8644b391c678315ea094e6dea
Adi <aditya7iyengar@gmail.com>
2018-01-08 01:22:38 -0500

    Remove the much deprecated system var support

--- lib/rummage_ecto/config.ex
+++ lib/rummage_ecto/config.ex
@@ -79,22 +79,6 @@ defmodule Rummage.Ecto.Config do
     config(:repo, nil)
   end
 
-  @doc """
-  `resolve_system_config` returns a `system` variable set up with `var_name` key
-   or returns the specified `default` value. Takes in `arg` whose first element is
-   an atom `:system`.
-
-  ## Examples
-  Returns value corresponding to a system variable config or returns the `default` value:
-      iex> alias Rummage.Ecto.Config
-      iex> Config.resolve_system_config({:system, "some random config"}, "default")
-      "default"
-  """
-  @spec resolve_system_config(Tuple.t, term) :: {term}
-  def resolve_system_config({:system, var_name}, default) do
-    System.get_env(var_name) || default
-  end
-
   defp config do
     Application.get_env(:rummage_ecto, Rummage.Ecto, [])
   end
