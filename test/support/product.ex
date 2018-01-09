084c9961838e01424e4dce003eaa7fcf09bac62a
Adi <aditya7iyengar@gmail.com>
2018-01-08 19:22:16 -0500

    Add boolean field to product for tests

--- test/support/product.ex
+++ test/support/product.ex
@@ -1,9 +1,14 @@
 defmodule Rummage.Ecto.Product do
+  @moduledoc """
+  This is Product Ecto.Schema for testing Rummage.Ecto with float values
+  and boolean values
+  """
   use Ecto.Schema
 
   schema "products" do
     field :name, :string
     field :price, :float
+    field :available, :boolean
     belongs_to :category, Rummage.Ecto.Category
 
     timestamps()
