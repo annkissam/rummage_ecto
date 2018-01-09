08d0eec8f598ea6de1d4570bed631ea9e9c8b316
Adi <aditya7iyengar@gmail.com>
2018-01-08 19:22:33 -0500

    Add moduledoc to category

--- test/support/category.ex
+++ test/support/category.ex
@@ -1,4 +1,8 @@
 defmodule Rummage.Ecto.Category do
+  @moduledoc """
+  This is a Category Ecto.Schema for testing Rummage.Ecto with a nested
+  associations
+  """
   use Ecto.Schema
   use Rummage.Ecto
 
