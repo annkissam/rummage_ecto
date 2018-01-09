f97ebed9415f470ad46bb60cf78cd6a5e05d9881
Adi <aditya7iyengar@gmail.com>
2018-01-09 09:13:14 -0500

    Add KeySet Pagination to CustomHooks:
    - Add documentation
    - Add keyset logic
    - Add pk logic
    - Add custom docs

--- lib/rummage_ecto/custom_hooks/keyset_paginate.ex
+++ lib/rummage_ecto/custom_hooks/keyset_paginate.ex
@@ -1,9 +1,17 @@
 defmodule Rummage.Ecto.CustomHooks.KeysetPaginate do
   @moduledoc """
-  `Rummage.Ecto.CustomHooks.KeysetPaginate` is a custom paginate hook that comes shipped
-  with `Rummage.Ecto`.
+  `Rummage.Ecto.CustomHooks.KeysetPaginate` is an example of a Custom Hook that
+  comes with `Rummage.Ecto`.
 
-  This module can be used by overriding the default paginate module. This can be done
+  This module uses `keyset` pagination to add a pagination query expression
+  on top a given `Ecto.Queryable`.
+
+  For more information on Keyset Pagination, check this [article](http://use-the-index-luke.com/no-offset)
+
+  NOTE: This module doesn't return a list of entries, but a `Ecto.Query.t`.
+
+
+  This module can be used by overriding the default module. This can be done
   in the following ways:
 
   In the `Rummage.Ecto` call:
@@ -17,183 +25,303 @@ defmodule Rummage.Ecto.CustomHooks.KeysetPaginate do
   ```elixir
   config :rummage_ecto,
     Rummage.Ecto,
-    _paginate: Rummage.Ecto.CustomHooks.KeysetPaginate
+    paginate: Rummage.Ecto.CustomHooks.KeysetPaginate
+  ```
+
+  OR
+
+  When `using` Rummage.Ecto with an `Ecto.Schema`:
+  ```elixir
+  defmodule MySchema do
+    use Rummage.Ecto, repo: SomeRepo,
+      paginate: Rummage.Ecto.CustomHooks.KeysetPaginate
+  end
   ```
   """
 
-  import Ecto.Query
-  alias Rummage.Ecto.Config
+  use Rummage.Ecto.Hook
 
-  @behaviour Rummage.Ecto.Hook
+  import Ecto.Query
+
+  @expected_keys ~w(per_page page last_seen_pk pk)a
+  @err_msg "Error in params, No values given for keys: "
+
+  @per_page 10
 
   @doc """
-  Builds a paginate queryable on top of the given `queryable` from the rummage parameters
-  from the given `rummage` struct.
+  This is the callback implementation of `Rummage.Ecto.Hook.run/2`.
+
+  Builds a paginate `Ecto.Query.t` on top of a given `Ecto.Query.t` variable
+  with given `params`.
+
+  Besides an `Ecto.Query.t` an `Ecto.Schema` module can also be passed as it
+  implements `Ecto.Queryable`
+
+  Params is a `Map` which is expected to have the keys `#{Enum.join(@expected_keys, ", ")}`.
+
+  If an expected key isn't given, a `Runtime Error` is raised.
 
   ## Examples
-  When rummage struct passed doesn't have the key "paginate", it simply returns the
-  queryable itself:
+  When an empty map is passed as `params`:
 
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
-      iex> import Ecto.Query
       iex> KeysetPaginate.run(Parent, %{})
-      Parent
+      ** (RuntimeError) Error in params, No values given for keys: per_page, page, last_seen_pk, pk
 
-  When the queryable passed is not just a struct:
+  When a non-empty map is passed as `params`, but with a missing key:
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> KeysetPaginate.run(Parent, %{per_page: 10})
+      ** (RuntimeError) Error in params, No values given for keys: page, last_seen_pk, pk
+
+  When a valid map of params is passed with an `Ecto.Schema` module:
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> params = %{per_page: 10, page: 1, last_seen_pk: 0, pk: :id}
+      iex> KeysetPaginate.run(Rummage.Ecto.Product, params)
+      #Ecto.Query<from p in Rummage.Ecto.Product, where: p.id > ^0, limit: ^10>
+
+  When the `queryable` passed is an `Ecto.Query` variable:
 
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
       iex> import Ecto.Query
-      iex> queryable = from u in "parents"
-      #Ecto.Query<from p in "parents">
-      iex> KeysetPaginate.run(queryable, %{})
-      #Ecto.Query<from p in "parents">
+      iex> queryable = from u in "products"
+      #Ecto.Query<from p in "products">
+      iex> params = %{per_page: 10, page: 1, last_seen_pk: 0, pk: :id}
+      iex> KeysetPaginate.run(queryable, params)
+      #Ecto.Query<from p in "products", where: p.id > ^0, limit: ^10>
 
-  When rummage `struct` passed has the key `"paginate"`, but with a value of `%{}`, `""`
-  or `[]` it simply returns the `queryable` itself:
 
-      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
-      iex> import Ecto.Query
-      iex> KeysetPaginate.run(Parent, %{"paginate" => %{}})
-      Parent
+  More examples:
 
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
       iex> import Ecto.Query
-      iex> KeysetPaginate.run(Parent, %{"paginate" => ""})
-      Parent
+      iex> params = %{per_page: 5, page: 5, last_seen_pk: 25, pk: :id}
+      iex> queryable = from u in "products"
+      #Ecto.Query<from p in "products">
+      iex> KeysetPaginate.run(queryable, params)
+      #Ecto.Query<from p in "products", where: p.id > ^25, limit: ^5>
 
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
       iex> import Ecto.Query
-      iex> KeysetPaginate.run(Parent, %{"paginate" => []})
-      Parent
+      iex> params = %{per_page: 5, page: 1, last_seen_pk: 0, pk: :some_id}
+      iex> queryable = from u in "products"
+      #Ecto.Query<from p in "products">
+      iex> KeysetPaginate.run(queryable, params)
+      #Ecto.Query<from p in "products", where: p.some_id > ^0, limit: ^5>
 
-  When rummage struct passed has the key "paginate", with "per_page" and "page" keys
-  it returns a paginated version of the queryable passed in as the argument:
-
-      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
-      iex> import Ecto.Query
-      iex> rummage = %{"paginate" => %{"per_page" => "1", "page" => "1"}}
-      %{"paginate" => %{"page" => "1", "per_page" => "1"}}
-      iex> queryable = from u in "parents"
-      #Ecto.Query<from p in "parents">
-      iex> KeysetPaginate.run(queryable, rummage)
-      #Ecto.Query<from p in "parents", limit: ^1, offset: ^0>
-
-      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
-      iex> import Ecto.Query
-      iex> rummage = %{"paginate" => %{"per_page" => "5", "page" => "2"}}
-      %{"paginate" => %{"page" => "2", "per_page" => "5"}}
-      iex> queryable = from u in "parents"
-      #Ecto.Query<from p in "parents">
-      iex> KeysetPaginate.run(queryable, rummage)
-      #Ecto.Query<from p in "parents", limit: ^5, offset: ^5>
-
-  When no `"page"` key is passed, it defaults to `1`:
-
-      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
-      iex> import Ecto.Query
-      iex> rummage = %{"paginate" => %{"per_page" => "10"}}
-      %{"paginate" => %{"per_page" => "10"}}
-      iex> queryable = from u in "parents"
-      #Ecto.Query<from p in "parents">
-      iex> KeysetPaginate.run(queryable, rummage)
-      #Ecto.Query<from p in "parents", limit: ^10, offset: ^0>
   """
-  @spec run(Ecto.Query.t, map) :: {Ecto.Query.t, map}
-  def run(queryable, rummage) do
-    paginate_params = Map.get(rummage, "paginate")
+  @spec run(Ecto.Query.t(), map()) :: Ecto.Query.t()
+  def run(queryable, paginate_params) do
+    :ok = validate_params(paginate_params)
 
-    case paginate_params do
-      a when a in [nil, [], {}, [""], "", %{}] -> queryable
-      _ -> handle_paginate(queryable, paginate_params)
+    handle_paginate(queryable, paginate_params)
+  end
+
+  # Helper function which handles addition of paginated query on top of
+  # the sent queryable variable
+  defp handle_paginate(queryable, paginate_params) do
+    per_page = Map.get(paginate_params, :per_page)
+    last_seen_pk = Map.get(paginate_params, :last_seen_pk)
+    pk = Map.get(paginate_params, :pk)
+
+    queryable
+    |> where([p1, ...], field(p1, ^pk) > ^last_seen_pk)
+    |> limit(^per_page)
+  end
+
+  # Helper function that validates the list of params based on
+  # @expected_keys list
+  defp validate_params(params) do
+    key_validations = Enum.map(@expected_keys, &Map.fetch(params, &1))
+
+    case Enum.filter(key_validations, & &1 == :error) do
+      [] -> :ok
+      _ -> raise @err_msg <> missing_keys(key_validations)
     end
   end
 
+  # Helper function used to build error message using missing keys
+  defp missing_keys(key_validations) do
+    key_validations
+    |> Enum.with_index()
+    |> Enum.filter(fn {v, _i} -> v == :error end)
+    |> Enum.map(fn {_v, i} -> Enum.at(@expected_keys, i) end)
+    |> Enum.map(&to_string/1)
+    |> Enum.join(", ")
+  end
+
   @doc """
-  Implementation of `before_hook` for `Rummage.Ecto.CustomHooks.KeysetPaginate`. This function
-  takes a `queryable`, `rummage` struct and an `opts` map. Using those it calculates
-  the `total_count` and `max_page` for the paginate hook.
+  Callback implementation for `Rummage.Ecto.Hook.format_params/2`.
+
+  This function takes an `Ecto.Query.t` or `queryable`, `paginate_params` which
+  will be passed to the `run/2` function, but also takes a list of options,
+  `opts`.
+
+  The function expects `opts` to include a `repo` key which points to the
+  `Ecto.Repo` which will be used to calculate the `total_count` and `max_page`
+  for this paginate hook module.
+
 
   ## Examples
+
+  When a `repo` isn't passed in `opts` it gives an error:
+
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
       iex> alias Rummage.Ecto.Category
-      iex> KeysetPaginate.before_hook(Category, %{}, %{})
-      %{}
+      iex> KeysetPaginate.format_params(Category, %{per_page: 1, page: 1}, [])
+      ** (RuntimeError) Expected key `repo` in `opts`, got []
+
+  When `paginate_params` given aren't valid, it uses defaults to populate params:
 
       iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
       iex> alias Rummage.Ecto.Category
       iex> Ecto.Adapters.SQL.Sandbox.checkout(Rummage.Ecto.Repo)
-      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 1"})
-      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 2"})
-      iex> Rummage.Ecto.Repo.insert(%Category{category_name: "Category 3"})
-      iex> rummage = %{"paginate" => %{"per_page" => "1", "page" => "1"}}
-      iex> KeysetPaginate.before_hook(Category, rummage, %{})
-      %{"paginate" => %{"max_page" => "3", "page" => "1", "per_page" => "1", "total_count" => "3"}}
+      iex> KeysetPaginate.format_params(Category, %{}, [repo: Rummage.Ecto.Repo])
+      %{max_page: 0, page: 1, per_page: 10, total_count: 0, pk: :id,
+        last_seen_pk: 0}
+
+  When `paginate_params` and `opts` given are valid:
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> alias Rummage.Ecto.Category
+      iex> paginate_params = %{
+      ...>   per_page: 1,
+      ...>   page: 1
+      ...> }
+      iex> repo = Rummage.Ecto.Repo
+      iex> Ecto.Adapters.SQL.Sandbox.checkout(repo)
+      iex> KeysetPaginate.format_params(Category, paginate_params, [repo: repo])
+      %{max_page: 0, last_seen_pk: 0, page: 1,
+        per_page: 1, total_count: 0, pk: :id}
+
+  When `paginate_params` and `opts` given are valid:
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> alias Rummage.Ecto.Category
+      iex> paginate_params = %{
+      ...>   per_page: 1,
+      ...>   page: 1
+      ...> }
+      iex> repo = Rummage.Ecto.Repo
+      iex> Ecto.Adapters.SQL.Sandbox.checkout(repo)
+      iex> repo.insert!(%Category{category_name: "name"})
+      iex> repo.insert!(%Category{category_name: "name2"})
+      iex> KeysetPaginate.format_params(Category, paginate_params, [repo: repo])
+      %{max_page: 2, last_seen_pk: 0, page: 1,
+        per_page: 1, total_count: 2, pk: :id}
+
+  When `paginate_params` and `opts` given are valid and when the `queryable`
+  passed has a `primary_key` defaulted to `id`.
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> alias Rummage.Ecto.Category
+      iex> paginate_params = %{
+      ...>   per_page: 1,
+      ...>   page: 1
+      ...> }
+      iex> repo = Rummage.Ecto.Repo
+      iex> Ecto.Adapters.SQL.Sandbox.checkout(repo)
+      iex> repo.insert!(%Category{category_name: "name"})
+      iex> repo.insert!(%Category{category_name: "name2"})
+      iex> KeysetPaginate.format_params(Category, paginate_params, [repo: repo])
+      %{max_page: 2, last_seen_pk: 0, page: 1,
+        per_page: 1, total_count: 2, pk: :id}
+
+  When `paginate_params` and `opts` given are valid and when the `queryable`
+  passed has a custom `primary_key`.
+
+      iex> alias Rummage.Ecto.CustomHooks.KeysetPaginate
+      iex> alias Rummage.Ecto.Item
+      iex> paginate_params = %{
+      ...>   per_page: 1,
+      ...>   page: 2
+      ...> }
+      iex> repo = Rummage.Ecto.Repo
+      iex> Ecto.Adapters.SQL.Sandbox.checkout(repo)
+      iex> repo.insert!(%Item{item_id: 5})
+      iex> repo.insert!(%Item{item_id: 6})
+      iex> KeysetPaginate.format_params(Item, paginate_params, [repo: repo])
+      %{max_page: 2, last_seen_pk: 1, page: 2,
+        per_page: 1, total_count: 2, pk: :item_id}
+
   """
-  @spec before_hook(Ecto.Query.t, map, map) :: map
-  def before_hook(queryable, rummage, opts) do
-    paginate_params = Map.get(rummage, "paginate")
+  @spec format_params(Ecto.Query.t(), map(), keyword()) :: map()
+  def format_params(queryable, paginate_params, opts) do
+    paginate_params = populate_params(queryable, paginate_params, opts)
 
-    case paginate_params do
-      nil -> rummage
-      _ ->
-        total_count = get_total_count(queryable, opts)
-
-        {page, per_page} = parse_page_and_per_page(paginate_params, opts)
-
-        per_page = if per_page < 1, do: 1, else: per_page
-
-        max_page_fl = total_count / per_page
-        max_page = max_page_fl
-          |> Float.ceil
-          |> round
-
-        page = cond do
-          page < 1 ->  1
-          max_page > 0 && page > max_page -> max_page
-          true -> page
-        end
-
-        paginate_params = paginate_params
-          |> Map.put("page", Integer.to_string(page))
-          |> Map.put("per_page", Integer.to_string(per_page))
-          |> Map.put("total_count", Integer.to_string(total_count))
-          |> Map.put("max_page", Integer.to_string(max_page))
-
-        Map.put(rummage, "paginate", paginate_params)
+    case Keyword.get(opts, :repo) do
+      nil -> raise "Expected key `repo` in `opts`, got #{inspect(opts)}"
+      repo -> get_params(queryable, paginate_params, repo)
     end
   end
 
-  defp get_total_count(queryable, opts), do: length(apply(get_repo(opts), :all, [queryable]))
+  # Helper function that populate the list of params based on
+  # @expected_keys list
+  defp populate_params(queryable, params, opts) do
+    params = params
+      |> Map.put_new(:per_page, Keyword.get(opts, :per_page, @per_page))
+      |> Map.put_new(:pk, pk(queryable))
+      |> Map.put_new(:page, 1)
 
-  defp get_repo(opts) do
-    opts[:repo] || Config.repo
+    Map.put_new(params, :last_seen_pk, get_last_seen(params))
   end
 
-  defp parse_page_and_per_page(paginate_params, opts) do
-    per_page = paginate_params
-      |> Map.get("per_page", Integer.to_string(opts[:per_page] || Config.per_page))
-      |> String.to_integer
-
-    page = paginate_params
-      |> Map.get("page", "1")
-      |> String.to_integer
-
-    {page, per_page}
+  # Helper function which gets the default last_seen_pk from
+  # page and per_page
+  defp get_last_seen(params) do
+    Map.get(params, :per_page) * (Map.get(params, :page) - 1)
   end
 
-  defp handle_paginate(queryable, paginate_params) do
-    per_page = paginate_params
-      |> Map.get("per_page")
-      |> String.to_integer
+  # Helper function which gets formatted list of params including
+  # page, per_page, total_count and max_page keys
+  defp get_params(queryable, paginate_params, repo) do
+    per_page = Map.get(paginate_params, :per_page)
+    total_count = get_total_count(queryable, repo)
+    max_page = total_count
+      |> (& &1 / per_page).()
+      |> Float.ceil()
+      |> trunc()
 
-    page = paginate_params
-      |> Map.get("page", "1")
-      |> String.to_integer
+    %{page: Map.get(paginate_params, :page), pk: Map.get(paginate_params, :pk),
+      last_seen_pk: Map.get(paginate_params, :last_seen_pk),
+      per_page: per_page, total_count: total_count, max_page: max_page}
+  end
 
-    offset = per_page * (page - 1)
-
+  # Helper function which gets total count of a queryable based on
+  # the given repo.
+  # This excludes operations such as select, preload and order_by
+  # to make the query more effectient
+  defp get_total_count(queryable, repo) do
     queryable
-    |> limit(^per_page)
-    |> offset(^offset)
+    |> exclude(:select)
+    |> exclude(:preload)
+    |> exclude(:order_by)
+    |> get_count(repo, pk(queryable))
+  end
+
+  # This function gets count of a query and repo passed.
+  # When primary key passed is nil, it just gets all the elements
+  # and counts them, but when a primary key is passed it just counts
+  # the distinct primary keys
+  defp get_count(query, repo, nil) do
+    repo
+    |> apply(:all, [distinct(query, :true)])
+    |> Enum.count()
+  end
+  defp get_count(query, repo, pk) do
+    query = select(query, [s], count(field(s, ^pk), :distinct))
+    hd(apply(repo, :all, [query]))
+  end
+
+  # Helper function which returns the primary key associated with a
+  # Queryable.
+  defp pk(queryable) do
+    schema = is_map(queryable) && elem(queryable.from, 1) || queryable
+
+    case schema.__schema__(:primary_key) do
+      [] -> nil
+      list -> hd(list)
+    end
   end
 end
