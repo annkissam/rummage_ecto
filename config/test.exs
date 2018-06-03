f7a3c5f389a614dad42e32ecbda517f6836d51c6
Adi Iyengar <aditya7iyengar@gmail.com>
2018-06-03 08:03:05 -0400

    Universal config

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,7 @@ config :rummage_ecto, ecto_repos: [Rummage.Ecto.Repo]
 
 config :rummage_ecto, Rummage.Ecto.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USERNAME"),
+  username: System.get_env("POSTGRES_USER"),
   password: System.get_env("POSTGRES_PASSWORD"),
   database: "rummage_ecto_test",
   pool: Ecto.Adapters.SQL.Sandbox
